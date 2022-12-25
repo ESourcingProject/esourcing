4395e8a77bde8b89e7aba7d3fa15a90157aa0d6f
Mücahit YILDIRIM <46230472+MucahitYILDIRIM@users.noreply.github.com>
2022-12-25 21:08:29 +0300

    Mob,le Icon Library Updated

--- ESoucingMobile/Screens/Home.js
+++ ESoucingMobile/Screens/Home.js
@@ -1,21 +1,29 @@
 import * as React from 'react';
 import { Stack,HStack, Button,Icon } from 'native-base';
-import { Ionicons,MaterialIcons } from "react-native-svg";
+import Feather from "react-native-vector-icons/Feather";
+import Entypo from "react-native-vector-icons/Entypo";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+
+
+
+
+
 
 const Home = ({ route,navigation }) => {
   const { userId } = route.params;
   return (
     <>
     <HStack space={1} mt="2">
-      <Button ml="3" minWidth="48" minHeight="24" onPress={() => navigation.navigate('ProductsScreen', {userId : userId})} leftIcon={<Icon as={MaterialIcons} name="battery-full" size="md" color="red.200" />}>
+      <Button ml="3" minWidth="48" minHeight="24" onPress={() => navigation.navigate('ProductsScreen', {userId : userId})} leftIcon={<Icon as={Feather} name="shopping-bag" size="md"  />}>
         Ürün İşlemleri
       </Button>
-      <Button mr="3" minWidth="48" minHeight="24" onPress={() => navigation.navigate('AuctionManagementScreen', {userId : userId} )} leftIcon={<Icon as={Ionicons} name="cloud-download-outline" size="md" />}>
+      <Button mr="3" minWidth="48" minHeight="24" onPress={() => navigation.navigate('AuctionManagementScreen', {userId : userId} )} leftIcon={<Icon as={Entypo} name="wallet" size="md" />}>
         İhale Yönetimi
       </Button>
     </HStack>
     <HStack space={1}  mt="2">
-      <Button ml="3" minWidth="48" minHeight="24" onPress={() => navigation.navigate('BidManagementScreen', {userId : userId} )} leftIcon={<Icon as={Ionicons} name="cloud-upload-outline" size="md" />}>
+      <Button ml="3" minWidth="48" minHeight="24" onPress={() => navigation.navigate('BidManagementScreen', {userId : userId} )} leftIcon={<Icon as={MaterialCommunityIcons} name="cash-plus" size="md" />}>
         İhale Teklif
       </Button>
     </HStack>
@@ -23,7 +31,7 @@ const Home = ({ route,navigation }) => {
     <Stack flex={1} flexDirection={'row'} alignItems={'flex-end'}>
       <Button colorScheme="secondary" m="3" width="96" maxHeight="12" onPress={() => navigation.reset({
         index: 0,
-        routes: [{ name: 'LoginScreen' }] })} leftIcon={<Icon as={Ionicons} name="cloud-upload-outline" size="md" />}>
+        routes: [{ name: 'LoginScreen' }] })} leftIcon={<Icon as={Entypo} name="log-out" size="md" />}>
         Çıkış Yap
       </Button>
     </Stack>
